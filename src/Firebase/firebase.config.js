7564194e6a251c6ec94eb9506cd22dbd7aff7ebf
mdshojib <xami7716@gmail.com>
2021-11-25 23:46:15 +0600

    transfrom

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,12 @@
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3rjacLhlxx0vzWgkLpcIeYsE-wPWv_ME",
-  authDomain: "set-login-from.firebaseapp.com",
-  projectId: "set-login-from",
-  storageBucket: "set-login-from.appspot.com",
-  messagingSenderId: "6231894115",
-  appId: "1:6231894115:web:68bc3fc60ac03c36b53056"
+  apiKey: "AIzaSyC5lwylhr76S0Aa88_jD3cWH7maCaO2YaQ",
+  authDomain: "trendy-travel-be200.firebaseapp.com",
+  projectId: "trendy-travel-be200",
+  storageBucket: "trendy-travel-be200.appspot.com",
+  messagingSenderId: "15948730323",
+  appId: "1:15948730323:web:362c5afa4642e81ebc6926",
+  measurementId: "G-ZNK60RB0CF"
 };
   
   export default firebaseConfig;
